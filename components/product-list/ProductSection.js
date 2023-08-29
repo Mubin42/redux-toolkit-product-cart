34fb8d5cba33fc55583c9c17bc8bc464d17ce603
Kazi Ehsanul Mubin <kazimubin.46@gmail.com>
2023-08-29 12:52:34 +0600

    first commit

---
+++ components/product-list/ProductSection.js
@@ -0,0 +1,12 @@
+import { Heading, Stack, Text } from '@chakra-ui/react';
+import React from 'react';
+
+const ProductSection = () => {
+  return (
+    <Stack w='full'>
+      <Heading>Product List</Heading>
+    </Stack>
+  );
+};
+
+export default ProductSection;
